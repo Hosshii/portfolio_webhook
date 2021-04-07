5d6fbeffd1a00a350c3eaafaacb605d353b16ac9
Hosshii <sao_heath6147.wistre@icloud.com>
2021-04-08 01:07:03 +0900

    todo

--- src/handler.rs
+++ src/handler.rs
@@ -1,8 +1,13 @@
-use std::net;
+use std::{future::Future, pin::Pin};
 
-use actix_web::{get, post, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
-use hmac::{Hmac, Mac, NewMac};
-use sha2::Sha256;
+use actix_web::{dev::Payload, error::ErrorUnauthorized, web::BytesMut, HttpMessage};
+use actix_web::{
+    get, post, web, App, Error, FromRequest, HttpRequest, HttpResponse, HttpServer, Responder,
+};
+// use futures_util::StreamExt;
+use hex::FromHex;
+use ring::{constant_time::verify_slices_are_equal, hmac};
+use std::iter::Iterator;
 
 // #[get("/")]
 // async fn hello() -> impl Responder {
@@ -20,21 +25,60 @@ use sha2::Sha256;
 
 const X_GITHUB_EVENT: &str = "X-Github-Event";
 const X_HUB_SIGNATURE: &str = "X-Hub-Signature-256";
+const SECRET: &str = &std::env::args().nth(1).unwrap();
 
 // async fn webhook(req: HttpRequest, body: String) -> impl Responder {
 //     let event = req.headers().get(X_GITHUB_EVENT);
 //     let signature = req.headers().get(X_HUB_SIGNATURE);
 // }
+struct Authorized;
 
-type HmacSha256 = Hmac<Sha256>;
+impl FromRequest for Authorized {
+    type Error = Error;
+    type Future = Pin<Box<dyn Future<Output = Result<Self, Self::Error>>>>;
+    type Config = ();
+
+    fn from_request(req: &HttpRequest, payload: &mut Payload) -> Self::Future {
+        let signature = &req
+            .headers()
+            .get(X_HUB_SIGNATURE)
+            .unwrap()
+            .to_str()
+            .unwrap();
+        // let mut bytes = web::BytesMut::new();
+        // while let Some(item) = body.next() {
+        //     bytes.extend_from_slice(&item?);
+        // }
+
+        // if verify_signature(SECRET, &payload, signature) {
+        //     ok(Authorized)
+        // } else {
+        //     err(ErrorUnauthorized("not authorized"))
+        // }
+        Box::pin(async move {
+            let mut body = BytesMut::new();
+            // let mut stream = req.take_payload();
+            while let Some(chunk) = payload.next().await {
+                body.extend_from_slice(&chunk?);
+            }
+            Ok(Authorized)
+        })
+    }
+}
+
 pub fn verify_signature(secret: &str, payload: &str, signature: &str) -> bool {
-    let signature = signature[7..signature.len()].as_bytes();
-    let mut mac = HmacSha256::new_varkey(secret.as_bytes()).expect("HMAC can take key of any size");
-    mac.update(payload.as_bytes());
-    let result = mac.clone().finalize().into_bytes();
-    println!("{:02x?}", result);
-    println!("{:?}", mac.verify(signature));
-    false
+    let secret = secret.as_bytes();
+    let payload = payload.as_bytes();
+    // let payload = payload[..payload.len() - 1].as_bytes();
+    let prefix = signature[7..signature.len()].as_bytes();
+    match Vec::from_hex(prefix) {
+        Ok(sig_bytes) => {
+            let key = hmac::Key::new(hmac::HMAC_SHA256, secret);
+            let tag = hmac::sign(&key, payload);
+            verify_slices_are_equal(tag.as_ref(), &sig_bytes).is_ok()
+        }
+        Err(_) => false,
+    }
 }
 // pub async fn init<A>(addr: A) -> std::io::Result<()>
 // where
