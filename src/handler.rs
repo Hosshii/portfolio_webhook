fb256e622cde51b188be1b0806c8ca171f6edac2
Hosshii <sao_heath6147.wistre@icloud.com>
2021-04-10 00:27:05 +0900

    add handler

--- src/handler.rs
+++ src/handler.rs
@@ -3,10 +3,10 @@ use crate::builder::MessageBuilder;
 use crate::error::MyError;
 use crate::utils::prelude::*;
 use crate::webhook::WebHook;
-use actix_web::{web, HttpRequest, HttpResponse, Responder};
+use actix_web::{web, HttpRequest, HttpResponse};
 use github_webhook::event::{
-    Event, IssueCommentEvent, IssuesAction, IssuesEvent, PullRequestEvent,
-    PullRequestReviewCommentEvent, PullRequestReviewEvent, PushEvent,
+    Event, IssueCommentEvent, IssuesEvent, PullRequestEvent, PullRequestReviewCommentEvent,
+    PullRequestReviewEvent, PushEvent,
 };
 use log::info;
 use std::rc::Rc;
@@ -44,6 +44,108 @@ async fn issue_handler(hook: &WebHook, event: IssuesEvent) -> Result<HttpRespons
         .issue()
         .action()
         .build();
+    let repo = ContentBuilder::new(Rc::clone(&event)).repo().build();
+
+    let message = MessageBuilder::new().title(title).repo(repo).build();
+
+    info!("msg {}", message);
+    // let res = hook.post_message(&message).await?;
+
+    // info!("{:?}", res);
+
+    Ok(HttpResponse::Ok().body("successfully posted"))
+}
+
+async fn issue_comment_handler(
+    hook: &WebHook,
+    event: IssueCommentEvent,
+) -> Result<HttpResponse, MyError> {
+    let event = Rc::new(EIssueComment(event));
+
+    let title = ContentBuilder::new(Rc::clone(&event))
+        .issue()
+        .action()
+        .build();
+    let repo = ContentBuilder::new(Rc::clone(&event)).repo().build();
+
+    let message = MessageBuilder::new().title(title).repo(repo).build();
+
+    info!("msg {}", message);
+    // let res = hook.post_message(&message).await?;
+
+    // info!("{:?}", res);
+
+    Ok(HttpResponse::Ok().body("successfully posted"))
+}
+
+async fn push_handler(hook: &WebHook, event: PushEvent) -> Result<HttpResponse, MyError> {
+    let event = Rc::new(EPush(event));
+
+    let title = ContentBuilder::new(Rc::clone(&event)).action().build();
+    let msg = ContentBuilder::new(Rc::clone(&event))
+        .commit()
+        .build_lines();
+    let repo = ContentBuilder::new(Rc::clone(&event)).repo().build();
+
+    let message = MessageBuilder::new()
+        .title(title)
+        .msg(msg)
+        .repo(repo)
+        .build();
+
+    info!("msg {}", message);
+    // let res = hook.post_message(&message).await?;
+
+    // info!("{:?}", res);
+
+    Ok(HttpResponse::Ok().body("successfully posted"))
+}
+
+async fn pull_request_handler(
+    hook: &WebHook,
+    event: PullRequestEvent,
+) -> Result<HttpResponse, MyError> {
+    let event = Rc::new(EPullRequest(event));
+
+    let title = ContentBuilder::new(Rc::clone(&event)).pr().action().build();
+    let repo = ContentBuilder::new(Rc::clone(&event)).repo().build();
+
+    let message = MessageBuilder::new().title(title).repo(repo).build();
+
+    info!("msg {}", message);
+    // let res = hook.post_message(&message).await?;
+
+    // info!("{:?}", res);
+
+    Ok(HttpResponse::Ok().body("successfully posted"))
+}
+
+async fn pull_request_review_handler(
+    hook: &WebHook,
+    event: PullRequestReviewEvent,
+) -> Result<HttpResponse, MyError> {
+    let event = Rc::new(EPullRequestReview(event));
+
+    let title = ContentBuilder::new(Rc::clone(&event)).pr().action().build();
+    let repo = ContentBuilder::new(Rc::clone(&event)).repo().build();
+
+    let message = MessageBuilder::new().title(title).repo(repo).build();
+
+    info!("msg {}", message);
+    // let res = hook.post_message(&message).await?;
+
+    // info!("{:?}", res);
+
+    Ok(HttpResponse::Ok().body("successfully posted"))
+}
+
+async fn pull_request_review_comment_handler(
+    hook: &WebHook,
+    event: PullRequestReviewCommentEvent,
+) -> Result<HttpResponse, MyError> {
+    let event = Rc::new(EPullRequestReviewComment(event));
+
+    let title = ContentBuilder::new(Rc::clone(&event)).pr().action().build();
     let repo = ContentBuilder::new(Rc::clone(&event)).repo().build();
 
     let message = MessageBuilder::new().title(title).repo(repo).build();
